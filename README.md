69c8b3c54c962c96f3a4767cf9c2ae5eee6766b3
Jacking Liu <jackingstarliu@gmail.com>
2016-07-14 19:24:35 +0800

    init project.

---
+++ README.md
@@ -0,0 +1,89 @@
+# AMD2CMD
+
+transform [AMD](https://github.com/amdjs/amdjs-api/wiki/AMD) or CommonJS inspired by [require.js](http://requirejs.org/) to [CommonJS](http://www.commonjs.org/).
+
+## Transform What
+
+before(AMD):
+
+```js
+define(['dep1', 'dep2'], function(dep1, dep2OtherName) {
+  return dep1 + dep2OtherName;
+});
+```
+
+transform after(CommonJS):
+
+```js
+var dep1 = require('dep1');
+var dep2OtherName = require('dep2');
+module.exports = dep1 + dep2OtherName;
+```
+
+* transform `define(['dep1', 'dep2'], function() {});` to `require('dep1');require('dep2');`
+* transform `define(function(){return statements;})` to `module.exports = statements;`
+* transform `require('obj/xxx')` to `require('../obj/xxx')`
+
+So, Notice this tool can not transform all require.js features.
+
+## Usage
+
+### cli
+
+First, install amd2cmd:
+
+```bash
+ npm install -g amd2cmd
+```
+
+Second, cd your project, and exec cmd:
+
+```bash
+ amd2cmd -in src/scripts/**/*.js -out build/scripts -baseDir src/scripts
+```
+
+### use with node.js
+
+First, install amd2cmd:
+
+```bash
+ npm install --save amd2cmd
+```
+
+Then, you can use amd2cmd like this:
+
+```js
+import { transform } from 'amd2cmd';
+
+transform({
+  in: 'src/scripts/**/*.js',
+  out: 'build/scripts',
+  baseDir: 'src/scripts',
+});
+```
+
+## scripts
+
+Build the project shell:
+
+```bash
+ $ npm build
+```
+
+Test the project shell:
+
+```bash
+ $ npm test
+```
+
+Test the project with coverage result:
+
+```bash
+ $ npm coverage
+```
+
+Generate JavaScript API doc:
+
+```bash
+ $ npm esdoc
+```
---
+++ gulpfile.babel.js
@@ -0,0 +1,188 @@
+'use strict';
+
+import gulp from 'gulp';
+
+import eslint from 'gulp-eslint';
+
+import mocha from 'gulp-mocha';
+import istanbul from 'gulp-istanbul';
+import { Instrumenter as ispartaInstrumenter } from 'isparta';
+
+import { rollup } from 'rollup';
+import babel from 'rollup-plugin-babel';
+import del from 'del';
+import rename from 'gulp-rename';
+import runSequence from 'run-sequence';
+import gulpBabel from 'gulp-babel';
+
+/**
+ * 清空非源码文件
+ */
+gulp.task('clean', ['build:clean'], callback => {
+  del(['reporters/**/*', 'reporters']).then(() => {
+    callback();
+  }, error => {
+    callback(error);
+  });
+});
+
+/**
+ * javascript代码静态检查
+ */
+gulp.task('lint', () => {
+  const srcPath = ['**/*.js', '!node_modules/**', '!dist/**', '!build/**', '!reporters/**'];
+  return gulp.src(srcPath)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+/**
+ * 执行单元测试
+ */
+gulp.task('unittest', () => {
+  const testSrcPath = ['test/**/*.spec.js'];
+  return gulp.src(testSrcPath)
+    .pipe(mocha({
+      require: ['chai'],
+    }));
+});
+
+/**
+ * 单元测试覆盖率前期准备
+ */
+gulp.task('unittest:coverage:pre', () => {
+  const srcPath = ['src/**/*.js'];
+  return gulp.src(srcPath)
+    .pipe(istanbul({
+      includeUntested: false,
+      instrumenter: ispartaInstrumenter,
+    }))
+    .pipe(istanbul.hookRequire());
+});
+
+/**
+ * 单元测试覆盖率
+ */
+gulp.task('unittest:coverage', ['unittest:coverage:pre'], () => {
+  const testSrcPath = ['test/**/*.spec.js'];
+  return gulp.src(testSrcPath)
+    .pipe(mocha({
+      require: ['chai'],
+    }))
+    .pipe(istanbul.writeReports({
+      dir: 'reporters/coverage',
+      reporters: ['lcov', 'json', 'text', 'text-summary'],
+    }))
+    .pipe(istanbul.enforceThresholds({
+      thresholds: {
+        global: {
+          statements: 90,
+          branches: 85,
+          lines: 90,
+          functions: 90,
+        },
+      },
+    }));
+});
+
+gulp.task('test', ['unittest']);
+gulp.task('coverage', ['unittest:coverage']);
+
+/**
+ * 自动执行单元测试和代码检查
+ */
+gulp.task('watch', () => {
+  const srcPath = ['/*.js', 'src/**/*.js', 'test/**/*.js'];
+  return gulp.watch(srcPath, ['lint', 'unittest']);
+});
+
+/**
+ * 项目构建
+ */
+gulp.task('build', callback => {
+  process.env.BABEL_DISABLE_CACHE = true;
+  runSequence('build:clean', ['build:src-copy', 'build:generate-rollup-babelrc'],
+    ['build:es5', 'build:es6'], 'build:clean-temp', callback);
+});
+
+/**
+ * 清空临时文件和构建文件
+ */
+gulp.task('build:clean', done => {
+  del(['build/**/*', 'build', 'dist/**/*', 'dist']).then(() => {
+    done();
+  }, error => {
+    done(error);
+  });
+});
+
+/**
+ * 清空临时文件
+ */
+gulp.task('build:clean-temp', done => {
+  del(['build/**/*', 'build']).then(() => {
+    done();
+  }, error => {
+    done(error);
+  });
+});
+
+/**
+ * js合并、编译成es5
+ */
+gulp.task('build:es5', () => {
+  const entryJsFilePath = 'build/src/main.js';
+  return rollup({
+    entry: entryJsFilePath,
+    plugins: [
+      babel({
+        runtimeHelpers: true,
+      }),
+    ],
+  }).then(bundle => {
+    bundle.write({
+      format: 'cjs',
+      dest: 'dist/index.js',
+    });
+  });
+});
+
+/**
+ * js合并、编译成EcmaScript 2015
+ */
+gulp.task('build:es6', () => {
+  const entryJsFilePath = 'build/src/main.js';
+  return rollup({
+    entry: entryJsFilePath,
+  }).then(bundle => {
+    bundle.write({
+      format: 'es6',
+      dest: 'dist/index.es6.js',
+    });
+  });
+});
+
+/**
+ * 拷贝源码
+ */
+gulp.task('build:src-copy', () =>
+  gulp.src('src/**/*')
+    .pipe(gulp.dest('build/src'))
+);
+
+/**
+ * 产生用于rollup的.babelrc文件
+ */
+gulp.task('build:generate-rollup-babelrc', () =>
+  gulp.src('scripts/rollup.babelrc')
+    .pipe(rename('.babelrc'))
+    .pipe(gulp.dest('build/src/'))
+);
+
+gulp.task('babelbuild', () => gulp.src(['p2pServer.js', 'p2pClient.js'])
+  .pipe(gulpBabel({ presets: ['es2015'] })).pipe(gulp.dest('dist')));
+
+gulp.task('default', done => {
+  runSequence('lint', 'test', 'build', done);
+});
